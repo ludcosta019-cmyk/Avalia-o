368ff06e3eb2b0d816e1f5331aa0b3454b45aedd
ludcosta019-cmyk <ludcosta019@gmail.com>
2026-08-11 20:35:31 -0300

    Finalização

---
+++ avaliacao.py
@@ -0,0 +1,10 @@
+#funcao para calculo
+def calcular_tempo_download():
+    #float pede quantidade decimal e input pede para o usuario digitar o valor
+    tamanho = float(input("Digite o tamanho do arquivo (MB): "))
+    velocidade = float(input("Digite a velocidade (Mbps): "))
+#calculo em segundos, dividindo o tamanho do arquivo pela velocidade convertida para MB/s
+    print(f"O tempo de download do arquivo é: {tamanho / (velocidade / 8)} segundos")
+
+#chama a funcao para calcular o tempo de download
+calcular_tempo_download()
